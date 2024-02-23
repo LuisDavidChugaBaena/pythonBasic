144bab52d01f858707524c34850e599da1ab83b0
LuisDavidChugaBaena <160773348+LuisDavidChugaBaena@users.noreply.github.com>
2024-02-23 14:58:15 -0500

    algoritmo9

---
+++ algoritmo9.py
@@ -0,0 +1,5 @@
+
+tabla = int(input("ingrese tabla"))
+multiplicar=  1 * tabla, 2 * tabla, 3 * tabla, 4 * tabla, 5 * tabla, 6 * tabla, 7 * tabla, 8 * tabla, 9 * tabla, 10 * tabla 
+resultado=(tabla * multiplicar)
+print("el resultado es", resultado)
