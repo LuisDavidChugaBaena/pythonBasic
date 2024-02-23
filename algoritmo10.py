bd2757c3b7a1e6f86da44396752876fe44031bf4
LuisDavidChugaBaena <160773348+LuisDavidChugaBaena@users.noreply.github.com>
2024-02-23 15:12:37 -0500

    algorutmo10

---
+++ algoritmo10.py
@@ -0,0 +1,13 @@
+
+nota1 = int(input("ingrese nota 1:")) 
+nota2 = int(input("ingrese nota 2:")) 
+nota3 = int(input("ingrese nota 3:")) 
+nota4 = int(input("ingrese nota 4:")) 
+nota5 = int(input("ingrese nota 5:")) 
+nota6 = int(input("ingrese nota 6:")) 
+resultado=(nota1+nota2+nota3+nota4+nota5+nota6)
+promedio = (resultado/6)
+print("el promedio de las 6 notas es:", promedio)
+
+
+
